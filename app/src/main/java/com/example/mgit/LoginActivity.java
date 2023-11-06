c29d937da0ff2ae48c419128e479df10e440cbe5
Peter Jung <peterj0117@naver.com>
2023-11-07 07:59:15 +0900

    로그링 화면 UI 3rd commit

---
+++ app/src/main/java/com/example/mgit/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.mgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class LoginActivity extends AppCompatActivity {
+    TextView sign;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        sign = findViewById(R.id.signin);
+
+        sign.setOnClickListener(v-> {
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        });
+    }
+}
