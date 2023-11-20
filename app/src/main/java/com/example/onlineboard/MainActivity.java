239df93014fc3a07c05ba4bb92a4263ad2f758d3
ydh20 <dubsteb@naver.com>
2023-11-21 00:59:28 +0900

    기존의 온라인 board를 변형하여 메모장으로 쓸 수 있게끔 변경함, 다수 기능 변경 및 코드에서 다량의 수정

--- app/src/main/java/com/example/onlineboard/MainActivity.java
+++ app/src/main/java/com/example/onlineboard/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,35 +20,40 @@ public class MainActivity extends AppCompatActivity {
 
     static ArrayList<Post> testDataSet; // Post 클래스를 저장하는 ArrayList, DB 연동시 변경 예정
     static CustomAdapter customAdapter; // RecyclerView 관리를 위한 Adapter
-    int newDataNumber = 21; // 임시로 사용하는 데이터 수 번호
+    static final int CREATE_POST_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { // 화면 생성
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        } // 상단 바 숨김
 
-        //----- 테스트를 위한 더미 데이터 생성 --------------------
+
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
+        //----- 테스트를 위한 더미 데이터 생성 --------------------
+
         testDataSet = new ArrayList<>(); // ArrayList 할당
-        for (int i = 1; i<=20; i++) { // 20개의 포스트 임시 생성
-            String id = "" + i; String title = "제목" + i;
-            String content = "test" + i;    String author = "작가" + i;
-            // post 요소들 (id, 제목, 내용, 작성자) 설정
-            long currentTimeMillis = System.currentTimeMillis(); // 시간 정보 저장
+
+        int id = 0; String title = "안녕하세요?";
+        String content = "당신이 원하는 내용을 작성하고 정리하여 기록하세요!";    String author = "개발자";
+        // post 요소들 (id, 제목, 내용, 작성자) 설정
+        long currentTimeMillis = System.currentTimeMillis(); // 시간 정ㄷ보 저장
 
 
-            // 밀리초를 Date 객체로 변환
-            Date date = new Date(currentTimeMillis);
+        // 밀리초를 Date 객체로 변환
+        Date date = new Date(currentTimeMillis);
 
-            // Date 객체를 원하는 형식의 문자열로 변환
-            String timeStamp = dateFormat.format(date);
+        // Date 객체를 원하는 형식의 문자열로 변환
+        String timeStamp = dateFormat.format(date);
 
-            // post 객체 생성한 후 ArrayList에 추가
-            Post post = new Post(id, title, content, author, timeStamp);
-            testDataSet.add(post);
-        }
+        // post 객체 생성한 후 ArrayList에 추가
+        Post post = new Post(id, title, content, timeStamp);
+        testDataSet.add(post);
         //--------------------------------------------------------
 
         // recyclerView 객체
@@ -72,42 +79,6 @@ public class MainActivity extends AppCompatActivity {
         //==========================================================
         recyclerView.setAdapter(customAdapter); // 어댑터 설정
 
-
-        //----- 데이터 추가/삭제 버튼, 임시 테스트용 -------------------------------
-        Button buttonAddItem = findViewById(R.id.buttonAddItem);
-        Button buttonDeleteItem = findViewById(R.id.buttonDeleteItem);
-        // 추가 버튼과 삭제 버튼
-
-        buttonAddItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String id = "" + newDataNumber; String title = "제목" + newDataNumber;
-                String content = "test" + newDataNumber;    String author = "작가" + newDataNumber;
-                long currentTimeMillis = System.currentTimeMillis();
-
-                // 밀리초를 Date 객체로 변환
-                Date date = new Date(currentTimeMillis);
-
-                // Date 객체를 원하는 형식의 문자열로 변환
-                String formattedDate = dateFormat.format(date);
-                String timeStamp = "현재 시간: " + formattedDate;
-
-                Post post = new Post(id, title, content, author, timeStamp);
-
-                testDataSet.add(post);
-                customAdapter.notifyItemInserted(testDataSet.size());
-                newDataNumber++;
-            }
-        });
-
-        buttonDeleteItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                testDataSet.remove(testDataSet.size()-1);
-                customAdapter.notifyDataSetChanged();
-                newDataNumber--;
-            }
-        });
         //-----------------------------------------------------------
         Button createPostButton = findViewById(R.id.reg_button1); // 글 작성 버튼
         createPostButton.setOnClickListener(new View.OnClickListener() {
@@ -115,9 +86,43 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // "글 작성" 버튼을 클릭하면 CreatePostActivity로 이동
                 Intent intent = new Intent(MainActivity.this, CreatePostActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, CREATE_POST_REQUEST); // Start activity for result
             }
         }); // 글 작성 버튼 누를 시 처리
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CREATE_POST_REQUEST && resultCode == RESULT_OK && data != null) {
+
+            if (data.hasExtra("newPost")) {
+                Post newPost = (Post) data.getSerializableExtra("newPost");
+                testDataSet.add(newPost);
+                customAdapter.notifyDataSetChanged();
+            }
+
+            if (data.hasExtra("updatedPost")) {
+                Post updatedPost = (Post) data.getSerializableExtra("updatedPost");
+                // Update the post in the testDataSet
+                int index = findPostIndexById(updatedPost.getPost_id());
+                if (index != -1) {
+                    testDataSet.set(index, updatedPost);
+                    customAdapter.notifyDataSetChanged();
+                }
+            }
+        }
+
+    }
+    
+    private int findPostIndexById(int postId) {
+        for (int i = 0; i < testDataSet.size(); i++) {
+            if (testDataSet.get(i).getPost_id() == postId) {
+                return i;
+            }
+        }
+        return -1; // 포스트 발견 실패시
     }
 }
